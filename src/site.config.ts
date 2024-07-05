26de22eca9e34f18b2b5b49eb5f8f4f3feca2649
Ronald Mindiola <ronaldmindiola@live.com>
2024-07-05 18:05:25 -0500

    Init

---
+++ src/site.config.ts
@@ -0,0 +1,14 @@
+export const metadata = {
+  title: "AstroFull",
+  description: "The official website of Astro",
+  charset: "utf-8",
+  lang: "en",
+  defaultLanguage: "en-us",
+  locale: "en_US",
+  type: "website",
+  url: "https://astro-logos.netlify.app",
+};
+
+export const layoutData = {
+  
+};
